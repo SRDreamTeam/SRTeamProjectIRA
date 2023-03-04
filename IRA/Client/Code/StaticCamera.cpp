5df42d63647593cea5cdc733ff203805764357b5
Impersy <duddjqkrtk2@naver.com>
2023-03-04 16:12:46 +0900

    이종혁 / 플레이어 기본 작업 1. Tranform 클래스에 함수 추가함 2. Terrain 오류 수정함 3. Player 수정함

--- IRA/Client/Code/StaticCamera.cpp
+++ IRA/Client/Code/StaticCamera.cpp
@@ -6,6 +6,7 @@
 CStaticCamera::CStaticCamera(LPDIRECT3DDEVICE9 pGraphicDev)
 	: CCamera(pGraphicDev)
 {
+
 }
 
 
@@ -32,7 +33,12 @@ HRESULT CStaticCamera::Ready_GameObject(const _vec3* pEye,
 	m_fNear = fNear;
 	m_fFar = fFar;
 
+
+	m_fAngle = D3DXToRadian(320.f);
+	m_fDistance = 15.f;
+
 	FAILED_CHECK_RETURN(__super::Ready_GameObject(), E_FAIL);
+
 
 	return S_OK;
 }
@@ -41,6 +47,7 @@ HRESULT CStaticCamera::Ready_GameObject(const _vec3* pEye,
 _int CStaticCamera::Update_GameObject(const _float& fTimeDelta)
 {
 	Key_Input(fTimeDelta);
+
 	Target_Renewal();
 
 	_int iExit = __super::Update_GameObject(fTimeDelta);
@@ -77,11 +84,14 @@ void CStaticCamera::Target_Renewal(void)
 	m_vEye = vLook * -1.f;
 	D3DXVec3Normalize(&m_vEye, &m_vEye);
 
-	m_vEye.y += 1.f;
+	m_vEye.y += 2.f;
 	m_vEye *= m_fDistance;
 
 	_vec3	vRight;
 	memcpy(vRight, &pPlayerTransform->m_matWorld.m[0][0], sizeof(_vec3));
+
+	if (vRight.x < 0)
+		vRight.x *= -1.f;
 
 	_matrix		matRot;
 	D3DXMatrixRotationAxis(&matRot, &vRight, m_fAngle);
@@ -90,6 +100,56 @@ void CStaticCamera::Target_Renewal(void)
 	m_vEye += pPlayerTransform->m_vInfo[INFO_POS];
 	m_vAt = pPlayerTransform->m_vInfo[INFO_POS];
 }
+
+
+void CStaticCamera::Mouse_Move(void)
+{
+	_long	dwMouseMove = 0;
+
+	_matrix		matCamWorld;
+	D3DXMatrixInverse(&matCamWorld, 0, &m_matView);
+
+	if (dwMouseMove = Engine::Get_DIMouseMove(DIMS_Y))
+	{
+		_vec3		vRight;
+		memcpy(&vRight, &matCamWorld.m[0][0], sizeof(_vec3));
+
+		_matrix		matRot;
+		D3DXMatrixRotationAxis(&matRot, &vRight, D3DXToRadian(dwMouseMove / 10.f));
+
+		memcpy(&matRot._41, &m_vAt, sizeof(_vec3));
+
+		_vec3	vLook = m_vEye - m_vAt;
+
+		D3DXVec3TransformCoord(&m_vEye, &vLook, &matRot);
+
+		//m_vAt = m_vEye + vLook;
+	}
+
+	if (dwMouseMove = Engine::Get_DIMouseMove(DIMS_X))
+	{
+		_vec3		vUp = { 0.f, 1.f, 0.f };
+
+
+		_matrix		matRot;
+		D3DXMatrixRotationAxis(&matRot, &vUp, D3DXToRadian(dwMouseMove / 10.f));
+
+		memcpy(&matRot._41, &m_vAt, sizeof(_vec3));
+
+		_vec3	vLook = m_vEye - m_vAt;
+
+		D3DXVec3TransformCoord(&m_vEye, &vLook, &matRot);
+
+		//m_vAt = m_vEye + vLook;
+	}
+
+}
+
+
+
+
+
+
 
 CStaticCamera* CStaticCamera::Create(LPDIRECT3DDEVICE9 pGraphicDev, const _vec3* pEye, const _vec3* pAt, const _vec3* pUp, const _float& fFov, const _float& fAspect, const _float& fNear, const _float& fFar)
 {
--- IRA/Client/Header/Player.h
+++ IRA/Client/Header/Player.h
@@ -15,6 +15,17 @@ class CPlayer : public Engine::CGameObject
 {
 	enum {PLAYER_HEAD,PLAYER_FACE,PLAYER_END};
 
+	enum {STAND,RUN,MOVE_ATTACK,STAND_ATTACK,DEATH,DASH,STATE_END};
+
+	enum { STAND_000, STAND_045, STAND_090, STAND_135, STAND_180, STAND_END };
+	enum { RUN_000, RUN_045, RUN_090, RUN_135, RUN_180, RUN_END };
+	enum { MOVE_ATTACK_045,MOVE_ATTACK_135, MOVE_ATTACK_END };
+	enum { STAND_ATTACK_045,STAND_ATTACK_135,STAND_ATTACK_END };
+	enum { DASH_045, DASH_135, DASH_END };
+	enum { ANGLE_000, ANGLE_045, ANGLE_090, ANGLE_135, ANGLE_180, ANGLE_225, ANGLE_270, ANGLE_315, ANGLE_STATE_END };
+	enum { ATTACK_ANGLE_045, ATTACK_ANGLE_135, ATTACK_ANGLE_225, ATTACK_ANGLE_315, ATTACK_ANGLE_STATE_END };
+	
+	
 private:
 	explicit CPlayer(LPDIRECT3DDEVICE9 pGraphicDev);
 	explicit CPlayer(const CPlayer& rhs);
@@ -31,14 +42,52 @@ private:
 	void			Key_Input(const _float& fTimeDelta);
 	void			SetUp_OnTerrain(void);
 	_vec3			Picking_OnTerrain(void);
+	void            Update_State();
+	_vec3           Get_World_Mouse(int x, int y);
+	void            Dash(const _float& fTimeDelta);
 
 private:
 	CRcTex*			m_pBufferCom;
 	CTransform*		m_pTransformCom;
-	CTexture*		m_pTextureCom;
+
+	CTexture*       m_pStandTextureCom[STAND_END] = {};
+	CTexture*       m_pRunTextureCom[RUN_END] = {};
+
+	CTexture*       m_pMoveAttackTextureCom[MOVE_ATTACK_END] = {};
+	CTexture*       m_pStandAttackTextureCom[STAND_ATTACK_END] = {};
+
+	CTexture*       m_pDashTextureCom[DASH_END] = {};
+
+	CTexture*		m_pDeathTextureCom;
+
 	CCalculator*	m_pCalculatorCom;
-	
-	_float			m_fSpeed = 10.f;
+
+private:	
+	_float			m_fSpeed = 20.f;
+
+	_float			m_fStandFrame = 0.f;
+	_float			m_fRunFrame = 0.f;
+	_float			m_fAttackFrame = 0.f;
+	_float			m_fDashFrame = 0.f;
+	_float			m_fDeathFrame = 0.f;
+
+	_int            m_iState = STAND;
+	_int            m_iPreState = STAND;
+
+	_int            m_iAngleState = ANGLE_000;
+	_int            m_iPreAngleState = ANGLE_000;
+
+	_int            m_iAttackAngleState = ATTACK_ANGLE_045;
+	_int            m_iPreAttackAngleState = ATTACK_ANGLE_045;
+
+	bool            m_bReverse = false;
+	bool            m_bAttackReverse = false;
+
+	_float          m_fAngle = 0.f;
+
+	_float          m_Dash_Time = 0.f;
+	bool            m_Is_Dash = false;
+
 
 public:
 	static CPlayer*		Create(LPDIRECT3DDEVICE9 pGraphicDev);
